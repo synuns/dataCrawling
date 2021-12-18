856d50836a7017992953fa35d460f78d5ab87fb8
Jdentity <identityjang@gmail.com>
2021-12-18 15:58:10 +0900

    [feat] for by date

--- Final/src/naverRankingNews.py
+++ Final/src/naverRankingNews.py
@@ -1,18 +1,30 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import pandas as pd
+from datetime import date, timedelta, datetime
 
-url = "https://news.naver.com/main/ranking/office.naver?officeId=020&date=20211217"
+officeId = {
+    "동아일보" : "020",
+    "조선일보" : "023",
+    "중앙일보" : "025",
+    "한겨레" : "028",
+    "경향일보" : "032",
+}
 
-html = urllib.request.urlopen(url)
-soup = BeautifulSoup(html, 'html.parser')
+today = datetime.today()
+numdays = 7
+date_list = [(today - timedelta(days=x)).strftime("%Y%m%d") for x in range(1, numdays)]
 
-rankingNews = soup.find('div', attrs={'class' : 'rankingnews_box_inner'})
-newsTitles = rankingNews.find_all('a', attrs={'class' : 'list_title nclicks(\'RBP.drnknws\')'})
-newsViews = rankingNews.find_all('span', attrs={'class' : 'list_view'})
+for day in date_list:
+    url = f'https://news.naver.com/main/ranking/office.naver?officeId=020&date={day}'
+    print(url)
+    html = urllib.request.urlopen(url)
+    soup = BeautifulSoup(html, 'html.parser')
 
-for newsTitle in newsTitles:
-    print(newsTitle.text)
-
-for newsView in newsViews:
-    print(newsView.text)
+    rankingNews = soup.find('div', attrs={'class' : 'rankingnews_box_inner'})
+    newsTitles = rankingNews.find_all('a', attrs={'class' : 'list_title nclicks(\'RBP.drnknws\')'})
+    newsViews = rankingNews.find_all('span', attrs={'class' : 'list_view'})
+    # for newsTitle in newsTitles:
+    #     print(newsTitle.text)
+    # for newsView in newsViews:
+    #     print(newsView.text)
